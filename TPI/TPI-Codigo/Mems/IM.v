ad0a7dbbd7cf1a7ab636dd41c7a5e792929e7341
BrisaAB <98664333+BrisaAB@users.noreply.github.com>
2023-11-04 19:34:42 -0300

    Update IM.v

--- TPI/TPI-Codigo/Mems/IM.v
+++ TPI/TPI-Codigo/Mems/IM.v
@@ -11,7 +11,41 @@ parameter ROM_ADDR_BITS = 5;
 reg [ROM_WIDTH-1:0] ROM [(2**ROM_ADDR_BITS)-1:0];
 
 initial begin
-    // LAS INSTRUCCIONES:
+   ROM[0] = 32'h100105b7
+   ROM[1] = 32'h0005a283
+   ROM[2] = 32'h00a00313
+   ROM[3] = 32'hfd028293
+   ROM[4] = 32'h00100393
+   ROM[5] = 32'h00500eb3
+   ROM[6] = 32'h00638863
+   ROM[7] = 32'h01d282b3
+   ROM[8] = 32'h00138393
+   ROM[9] = 32'hff5ff06f
+   ROM[10] = 32'h00458593
+   ROM[11] = 32'h0005ae03
+   ROM[12] = 32'hfd0e0e13
+   ROM[13] = 32'h01c284b3
+   ROM[14] = 32'h00858593
+   ROM[15] = 32'h0005a283
+   ROM[16] = 32'h00a00313
+   ROM[17] = 32'hfd028293
+   ROM[18] = 32'h00100393
+   ROM[19] = 32'h00500eb3
+   ROM[20] = 32'h00638863
+   ROM[21] = 32'h01d282b3
+   ROM[22] = 32'h00138393
+   ROM[23] = 32'hff5ff06f
+   ROM[24] = 32'h00458593
+   ROM[25] = 32'h0005ae03
+   ROM[26] = 32'hfd0e0e13
+   ROM[27] = 32'h01c28933
+   ROM[28] = 32'h00100393
+   ROM[29] = 32'h00900eb3
+   ROM[30] = 32'h01238863
+   ROM[31] = 32'h01d484b3
+   ROM[32] = 32'h00138393
+   ROM[33] = 32'hff5ff06f
+    /* // LAS INSTRUCCIONES:
     // addi x8,x0,3
     //imm[31:20] = 3
     //rs1[19:15] = 00000
@@ -31,7 +65,7 @@ initial begin
     ROM[20] = 32'b00000000000000000000000000010011;
     //addi x8,x8,2
     ROM[24] = 32'b00000000000001000000010000010011;
-    //...
+    //...*/
 end
 
 assign instr = ROM[pc];
