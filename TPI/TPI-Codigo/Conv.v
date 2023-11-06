2b5faae1e091d98649ed272ac9f7b0adc62c8b7b
francoherg <francoherg@gmail.com>
2023-11-05 23:04:02 -0300

    MMU, reestructuracion del mem.v

---
+++ TPI/TPI-Codigo/Conv.v
@@ -0,0 +1,44 @@
+module Top(
+    //Entradas
+    input wire clk,
+
+    //Salidas
+    output [31:0] sal,
+    output [15:0] addressSal
+);
+
+// ---------- Cableciños ----------
+wire[31:0] s_instr;
+wire[15:0] s_pc;
+wire s_WE;
+wire[15:0] s_address;
+wire[31:0] s_writeData;
+wire[31:0] s_readData;
+// ---------- ---------- ----------
+
+rv32i RV(
+    .clk(clk),
+    .instr(s_instr),
+    .readData(s_readData),
+    .pc(s_pc),
+    .WE(s_WE),
+    .address(s_address),
+    .writeData(s_writeData)
+);
+
+mem Memory(
+    .clk(clk),
+    .pc(s_pc),
+    .WE(s_WE),
+    .address(s_address),
+    .writeData(s_writeData),
+
+    //Salidas
+    .instr(s_instr),
+    .readData(s_readData)
+);
+
+assign sal = s_writeData;
+assign addressSal = s_address;
+
+endmodule
--- TPI/TPI-Codigo/mem.v
+++ TPI/TPI-Codigo/mem.v
@@ -1,15 +1,12 @@
-`include "Mems/DM.v"
-`include "Mems/IM.v"
-
 module mem(
-    //Entradas
+    // Entradas
     input wire clk,
     input [15:0] pc,
     input wire WE,
-    input [15:0] address,
+    input [15:0] address_virtual,
     input [31:0] writeData,
 
-    //Salidas
+    // Salidas
     output [31:0] instr,
     output [31:0] readData
 );
@@ -17,21 +14,56 @@ module mem(
 // ---------- Cableciños ----------
 wire[31:0] DEBUGinstr16;
 wire[31:0] DEBUGinstr20;
+wire[1:0] block_select;
+wire[15:0] address_physical;
+wire DataEnable;
+wire StackEnable; 
+wire MMIOEnable;
 // ---------- ---------- ----------
 
+// Memory Mapping (MMU)
+MMU memory_mapping(
+    .address_virtual(address_virtual),
+    .block_select(block_select),
+    .address_physical(address_physical),
+    .DataEnable(DataEnable),
+    .StackEnable(StackEnable),
+    .MMIOEnable(MMIOEnable)
+);
+
+// Data Memory
+DM dataMem(
+    .clk(clk),
+    .address(address_physical),
+    .wd(writeData),
+    .we(DataEnable), 
+    .rd(readData)
+);
+
+// Stack Memory
+DM stackMem(
+    .clk(clk),
+    .address(address_physical),
+    .wd(writeData),
+    .we(StackEnable),
+    .rd(readData)
+);
+
+// MMIO Memory
+DM MMIO(
+    .clk(clk),
+    .address(address_physical),
+    .wd(writeData),
+    .we(MMIOEnable), 
+    .rd(readData)
+);
+
+// Instruction Memory (IM)
 IM instrMem(
     .pc(pc),
     .instr(instr),
     .DEBUGinstr16(DEBUGinstr16),
     .DEBUGinstr20(DEBUGinstr20)
-);
-
-DM dataMem(
-    .clk(clk),
-    .address(address),
-    .wd(writeData),
-    .we(WE),
-    .rd(readData)
 );
 
 endmodule
